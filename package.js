4e94964222a2eccc1b4208135ba1ea470fed0e99
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-10-09 11:22:56 +0200

    Use Meteor 1.6 to avoid conflict with ecmascript version

--- package.js
+++ package.js
@@ -7,7 +7,7 @@ Package.describe({
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.8');
+  api.versionsFrom('1.6');
   api.use('ecmascript');
   api.use('accounts-base');
   api.use('check');
